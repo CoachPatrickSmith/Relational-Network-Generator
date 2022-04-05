d414438e90c38d4c171a125d7cc047a8a0725c80
Patrick Smith <patricksmith@Patricks-MacBook-Pro.local>
2022-04-04 17:19:08 -0700

    Added additional example sets to align with publication manuscript. Updated color palette for contrast and visual accessibility. Verified graph visualizer is still compatible after the v0.2 to v1.0 changes in the permutations code.

--- Network graph visualizer.R
+++ Network graph visualizer.R
@@ -3,46 +3,6 @@ library(igraph)
 library(dplyr)
 library(intergraph)
 
-# This funciton pulls the end vertex from the string
-parseTo<-function(a){
-  if(nchar(a)>3){substr(a,4,4)}
-  else{substr(a,3,3)
-  }
-}
-
-#This function pulls the relation type from the string
-parseRel<-function(a){
-  if(nchar(a)>3){substr(a,2,3)}
-  else{substr(a,2,2)
-  }
-}
-
-#This function outputs a color string for the edge based on the derivation level
-#This scheme should be red/green colorblind accesssible
-edgeColor<- function(a){
-  case_when(
-    a == "Directly Trained" ~ "Blue",
-    a == "Mutually Entailed"~ "Brown",
-    a == "Combinatorially Entailed" ~ "Dark Green"
-  )
-}
-
-#This takes the output from the relational permutation.R v0.1 script and 
-# parses start and end vertexes and relational types, and assigns edge color attributes.
-# It filters for duplicate instances of edges prior to assigning to a new data frame.
-# The output is a data frame formatted such that each row is an edge description and
-# can be coerced into the network data structure.
-
-relb%>%
-  mutate(From = unlist(substr(Relation,1,1)), 
-         To=unlist(lapply(Relation, parseTo)),
-         Relation_Type = unlist(lapply(Relation, parseRel)),
-         .before=Derivation_Level)%>%
-  mutate(edge_color = unlist(lapply(Derivation_Level, edgeColor)))%>%
-  filter(!Relation_Type=="ku")%>% #use this to supress "ku" relations.  Comment this line to show "ku" relations
-  filter(!duplicated(Relation))->
-  edgeList
-
 #This does the same for the v0.2 script, no mutate required since the newer script does the parsing already
 x%>%
   #filter(!Relation_Type=="ku")%>% #use this to supress "ku" relations. Comment this line to show "ku" relations
@@ -66,14 +26,73 @@ relTnet<- network(edgeList,
                   # different relation (e.g. A<B & AkuB)
                   # This may be a product of using an incoherent(not previously vetted for consistency) start list.  
                   # That may need to be explored independently.
+                  ##   Following up on above, this is not a logic error. 
+                  ##    Multiple relations between vertex is reasonable given the pairs of relations used to combine.
                   multiple = TRUE)
 
 rel_graph<- asIgraph(relTnet)
 
 plot(rel_graph,
-     vertex.size=8,
+     vertex.size=12,
      vertex.label=V(rel_graph)$vertex.names,
+     vertex.label.color = "white",
+     vertex.label.size = 20,
+     vertex.color = "black",
      edge.label=E(rel_graph)$Relation,
-     edge.color=E(rel_graph)$edge_color,
-     edge.arrow.size=1
+     edge.color=E(rel_graph)$EdgeColor,
+     #edge.arrow.size=1,
+     edge.weight=(1+E(rel_graph)$DerivationDegree)^-1,
+     edge.width = ((1+E(rel_graph)$DerivationDegree)^-1)*5
 )
+
+#### For making a plot image of the table
+# Only works for tables less than 30 rows.  
+#   I don't know why it clips tops and bottoms otherwise.
+# library(gridExtra)
+# x.table<-tableGrob(x)
+# grid.arrange(x.table)
+####
+
+
+
+#### Historical code for previous attempts
+# #### This function pulls the end vertex from the string
+# parseTo<-function(a){
+#   if(nchar(a)>3){substr(a,4,4)}
+#   else{substr(a,3,3)
+#   }
+# }
+# 
+# #This function pulls the relation type from the string
+# parseRel<-function(a){
+#   if(nchar(a)>3){substr(a,2,3)}
+#   else{substr(a,2,2)
+#   }
+# }
+# 
+# #This function outputs a color string for the edge based on the derivation level
+# #This scheme should be red/green colorblind accesssible
+# edgeColor<- function(a){
+#   case_when(
+#     a == "Directly Trained" ~ "Blue",
+#     a == "Mutually Entailed"~ "Brown",
+#     a == "Combinatorially Entailed" ~ "Dark Green"
+#   )
+# }
+# 
+# #This takes the output from the relational permutation.R v0.1 script and 
+# # parses start and end vertexes and relational types, and assigns edge color attributes.
+# # It filters for duplicate instances of edges prior to assigning to a new data frame.
+# # The output is a data frame formatted such that each row is an edge description and
+# # can be coerced into the network data structure.
+# 
+# # relb%>%
+# #   mutate(From = unlist(substr(Relation,1,1)), 
+# #          To=unlist(lapply(Relation, parseTo)),
+# #          Relation_Type = unlist(lapply(Relation, parseRel)),
+# #          .before=Derivation_Level)%>%
+# #   mutate(edge_color = unlist(lapply(Derivation_Level, edgeColor)))%>%
+# #   filter(!Relation_Type=="ku")%>% #use this to supress "ku" relations.  Comment this line to show "ku" relations
+# #   filter(!duplicated(Relation))->
+# #   edgeList
+# #####
--- relational permutations.R
+++ relational permutations.R
@@ -9,6 +9,8 @@ d <- c("A<B", "B<C", "C<D", "D<E", "E<F", "F<G")
 e <- c("A<B", "B<C")
 f <- c("A<B", "B>C", "C=D")
 g<- c("c<a","b<a","e<d","f<d","d<a")
+h<- c("A<B", "B=C")
+i<- c("A=B", "B=C")
 t<- c("gkuj", "h<i", "i<k", "k<j", "l<o", "mkuk", "n=o", "p>o")
 u<-c("A<B", "B>C", "C<D", "E>D", "F>E", "F<G","g=j", "h<i", "i<k", "k<j", "l<o", "m=k", "n=o", "p>o", "D=m","i=o")
 #The a,b,c, and t variables above are examples of lists of relational statements.
@@ -18,7 +20,7 @@ u<-c("A<B", "B>C", "C<D", "E>D", "F>E", "F<G","g=j", "h<i", "i<k", "k<j", "l<o",
 # Running the v0.2 code just below will output an edge list table ready to be imported into a (network) or (iGraph) data structure.
 #   Running the Network Graph visualizer.R script will produce a visual graph plot of the trained and derived network.
 
-#### Version 1.0
+## Version 1.0
 #relationParse input 1 value (relStatement)
 ##  and output 8 values in 1 row
 ##      (From(Stim1), Relation, To(Stim2), Relation, Relation Type, Edge Color, Derivation Level, Derivation Degree, Derived From{defaul none})
@@ -63,7 +65,7 @@ mutualEntail <- function(from, relation, to, derivationDegree=0){
              Relation=paste(to, r, from, sep = ""),
              DerivationLevel="Mutually Entailed",
              DerivationDegree = derivationDegree+1,
-             EdgeColor="Deep Red",
+             EdgeColor="Purple",
              DerivedFrom = paste(from, relation, to, sep = "")
   )
 }
@@ -149,12 +151,14 @@ combinatorialEntail <- function(From1, Relation1, To1, derivationDegree1=0, From
                  ## That arbitrary distinction has interesting implications on if a mutually entailed relation has the same 
                  ## probability of response as a combinatorially entailed response from the same directly trained relations.
                  ## That assumes that derivation degree is a metaphorical measure of behavioral probability. (Higher degree = Lower Probability)
-                 EdgeColor="Deep Orange",
+                 EdgeColor="Orange",
                  DerivedFrom = paste(e, r1, g,",",h,r2,j, sep = "")
   )
   x<- mutualEntail(from = w$From, relation = w$RelationType, to= w$To, derivationDegree = w$DerivationDegree-1)
-  # Related to the above note regarding derivation degree, this d-11 assignment ensures that mutually combinatorially entailed relations
+  # Related to the above note regarding derivation degree, this d-1 assignment ensures that mutually combinatorially entailed relations
   ## are assigned the same derivation degree value as the combinatorially entailed relation they are derived from.
+  x$DerivationLevel <- "Combinatorially Mutually Entailed"
+  x$EdgeColor <- "Orange"
   rbind(w,x)
   
 }
@@ -325,257 +329,8 @@ relationTrain <- function(relationList, maxDerivationDegree = 2){
 
 
 
-x<-relTrain(b) #use the variable name "x" to pass to the visualizer without issue
+x<-relationTrain(i, maxDerivationDegree = 5) #use the variable name "x" to pass to the visualizer without issue
 
 
-# 
-# #### Version 0.2
-# library(tidyr)
-# library(combinat)
-# library(dplyr)
-# mutEnt<- function(a){
-#   b<- substr(a,1,1)
-#   r<- case_when(
-#     nchar(a)==3 ~ substr(a,2,2),
-#     nchar(a)==4 ~ substr(a,2,3)
-#     )
-#   d<-  case_when(
-#     nchar(a)==3 ~ substr(a,3,3),
-#     nchar(a)==4 ~ substr(a,4,4)
-#     )
-#   s<- case_when(
-#     r=="="~r,
-#     r=="<"~">",
-#     r==">"~"<",
-#     r=="ku"~r
-#   )
-#   data.frame(From=c(b,d),
-#              To=c(d,b),
-#              Relation_Type=c(r,s),
-#              Derivation_Level=c("Directly Trained", "Mutually Entailed"),
-#              Relation=c(paste(b,r,d, sep = ""), paste(d,s,b, sep = "")),
-#              edge_color=c("Blue", "Dark Red")
-#              )
-# }
-# combEnt<- function (a){
-#   b<-a[1]
-#   d<-a[2]
-#   e<-substr(b,1,1)
-#   r1<- case_when(
-#     nchar(b)==3 ~ substr(b,2,2),
-#     nchar(b)==4 ~ substr(b,2,3)
-#   )
-#   g<- case_when(
-#     nchar(b)==3 ~ substr(b,3,3),
-#     nchar(b)==4 ~ substr(b,4,4)
-#   )
-#   if(e==g){return()} #Testing first relational statement for identity
-#   h<-substr(d,1,1)
-#   r2<- case_when(
-#     nchar(d)==3 ~ substr(d,2,2),
-#     nchar(d)==4 ~ substr(d,2,3)
-#   )
-#   j<- case_when(
-#     nchar(d)==3 ~ substr(d,3,3),
-#     nchar(d)==4 ~ substr(d,4,4)
-#   )
-#   if(r1=="ku"){return()}
-#   if(r2=="ku"){return()}
-#   if(h==j){return()} #Testing second relational statement for identity
-#   if(h%in%c(e,g) & j%in%c(e,g)){return()} #Testing between relational statements for two of the same
-#   if(!(h%in%c(e,g)) & !(j%in%c(e,g))){return()} #Testing between relational statements for no shared stimuli (This may be a source for writing to a separate list that is recombined later)
-#   r3<-NA 
-#   r3<-if_else(e==h,
-#               if_else(r1==r2,if_else(r1=="=",r1, "ku"),
-#                       if_else(r1=="=", r2, if_else(r2=="=",if_else(r1=="<",">","<"),r2))),
-#               if_else(e==j,
-#                       if_else(r1==r2,if_else(r1=="=",r1,if_else(r1=="<",">","<")),
-#                               if_else(r1=="=",if_else(r2=="<",">","<"),
-#                                       if_else(r2=="=",if_else(r1=="<",">","<"),"ku"))),
-#                       if_else(g==j,
-#                               if_else(r1==r2, if_else(r1=="=", r1,"ku"),
-#                                       if_else(r1=="=", if_else(r2=="<",">","<"),
-#                                               r1 )),
-#                               if_else(r1==r2,r1,if_else(r1=="=",r2,if_else(r2=="=",r1,"ku"))))))
-#   if(is.na(r3)){return()}
-#   l<- if_else(e%in%c(h,j),g,e)
-#   m<- if_else(h%in%c(e,g),j,h)
-#   r4<- case_when(
-#     r3=="="~r3,
-#     r3=="<"~">",
-#     r3==">"~"<",
-#     r3=="ku"~r3
-#   )
-#   data.frame(From=c(l,m),
-#              To=c(m,l),
-#              Relation_Type=c(r3,r4),
-#              Derivation_Level=c("Combinatorially Entailed", "Combinatorially Mutually Entailed"),
-#              Relation=c(paste(l,r3,m, sep = ""), paste(m,r4,l, sep = "")),
-#              edge_color=c("Dark Green", "Dark Green"),
-#              Derived_from=c(paste(a[1],a[2], sep = ","),paste(a[1],a[2], sep = ","))
-#   )
-#   
-# } #this currently may not handle KU+nonKU relations correctly. Proceed with caution.
-# relTrain<- function(a){
-#   if(length(a)<2){return(mutEnt(a))} #Handling of single relational statements as a special case.
-#   u<-Reduce(function(...) merge(...,all=T),lapply(a, mutEnt))
-#   v<-combn(filter(u,Derivation_Level=="Directly Trained")[,5],m=2)
-#   w<-if(length(v)<=2){
-#     combEnt(as.list(v))
-#   }else{
-#     Reduce(function(...) merge(..., all=T),Filter(Negate(is.null), apply(v,2,combEnt)))
-#   }
-#   x<-full_join(u,w)
-#   
-#   ##The following rows should be repeated until diff.join is equal to zero.
-#   i<-1
-#   while (i>0) {
-#     y<-combn(filter(x,Derivation_Level=="Directly Trained"|Derivation_Level=="Combinatorially Entailed")[,5],m=2)
-#     w<-Reduce(function(...) merge(..., all=T),Filter(Negate(is.null), apply(y,2,combEnt)))
-#     pre.join<-nrow(x)
-#     x<-full_join(x,w)
-#     x%>%
-#       filter(!duplicated(Relation))->
-#       x
-#     diff.join<-nrow(x)-pre.join
-#     i<-diff.join}
-#   return(x)
-# } #This now does all of the below in v0.2 automatically.
-# 
-# x<-relTrain(b) #use the variable name "x" to pass to the visualizer without issue
+### Code testing
 
-# ###V0.2 code detailed commenting of the relTrain function internals above.############
-# ##The first instances of u,v,w, & x below should only be run once.
-# #This generates the mutually entailed relations from the trained set and translates the strings into data tables.
-# #   Change the input_data_set_variable in lapply(<input_data_set_variable>, mutEnt) to try different lists
-# u<-Reduce(function(...) merge(...,all=T),lapply(b, mutEnt))
-# #This pulls the directly trained relations from the above data tables 
-# # and creates a list of pairs of all the possible combinations of those directly trained.
-# v<-combn(filter(u,Derivation_Level=="Directly Trained")[,5],m=2)
-# #This takes those pairs and returns all the combinatorially derived, and mutually combinatorially derived relations.
-# w<-if(length(v)<=2){combEnt(as.list(v))}else{Reduce(function(...) merge(..., all=T),Filter(Negate(is.null), apply(v,2,combEnt)))}
-# #This merges rows not previously occuring in the original data tables from the combinatorial derivation table.
-# x<-full_join(u,w)
-# 
-# 
-# ##The following rows should be repeated until diff.join is equal to zero.
-# ## That will ensure that the possible combinations of the list have been fully derived through
-# #This pulls the unique relations that were directly trained of combinatorially derived
-# # and generates all possible pair combinations.
-# y<-combn(filter(x,Derivation_Level=="Directly Trained"|Derivation_Level=="Combinatorially Entailed")[,5],m=2)
-# #This takes those pairs and returns all the combinatorially derived, and mutually combinatorially derived relations.
-# w<-Reduce(function(...) merge(..., all=T),Filter(Negate(is.null), apply(y,2,combEnt)))
-# #This stores the pre-merged number of rows in the master table to check against if new unique rows were added.
-# pre.join<-nrow(x)
-# #This merges rows not previously occuring in the original data tables from the new combinatorial derivation table.
-# x<-full_join(x,w)
-# #This filters out all duplicate instances of relations that may be combinatorially and mutually combinatorially derived
-# # a second or more times.
-# ## Those second and more incidences may be worth counting toward a differential edge weight in the future.  
-# ##  They might provide insight into how networks morph as new information is incorporated or old information is reviewed.
-# x%>%
-#   filter(!duplicated(Relation))->
-#   x
-# #This compares the current number of rows in the master table against it's pre-joined and filtered state.
-# diff.join<-nrow(x)-pre.join
-# #Iterate from the y assignment line above through here until diff.join is equal to zero
-
-
-##### Version 0.1 below ############
-#In V0.1,longer lists(>3 elements) do not iterate through all possible combinatorially derived relations.
-
-# mutualEntail <- function(a){
-#   if("k" %in% substr(a,2,2)){return(paste(substr(a,4,4),"ku",substr(a,1,1), sep = ""))}
-#   x<- substr(a,1,1)
-#   y<- substr(a,3,3)
-#   z<- substr(a,2,2)
-#   w<- if_else(z=="=",
-#               "=",
-#               if_else(z== "<",
-#                       ">",
-#                       "<")
-#   )
-#   paste(y,w,x, sep = "")
-# }
-# 
-# combEntail <- function(a){
-#   e<-  if_else(substr(a[1],2,2) != "k",substr(a[1],1,1),substr(a[1],1,1))
-#   r1<- if_else(substr(a[1],2,2) != "k",substr(a[1],2,2),substr(a[1],2,3))
-#   f<- if_else(substr(a[1],2,2) != "k",substr(a[1],3,3),substr(a[1],4,4))
-#   if(e==f) {return()}
-#   g<- if_else(substr(a[2],2,2) != "k",substr(a[2],1,1),substr(a[2],1,1))
-#   r2<- if_else(substr(a[2],2,2) != "k",substr(a[2],2,2),substr(a[2],2,3))
-#   h<- if_else(substr(a[2],2,2) != "k",substr(a[2],3,3),substr(a[2],4,4))
-#   if(g==h) {return()}
-#   if(g%in%c(e,f) & h%in%c(e,f)){return()}
-#   r3<-NA
-#   if(TRUE%in%(c(g,h)%in%c(e,f))){
-#     r3<-  if_else(e==g,
-#                   if_else(r1==r2,if_else(r1=="=",r1, "ku"),
-#                           if_else(r1=="=", r2, if_else(r2=="=",if_else(r1=="<",">","<"),r2))),
-#                   if_else(e==h,
-#                           if_else(r1==r2,if_else(r1=="=",r1,if_else(r1=="<",">","<")),
-#                                   if_else(r1=="=",if_else(r2=="<",">","<"),
-#                                           if_else(r2=="=",if_else(r1=="<",">","<"),"ku"))),
-#                           if_else(f==g,
-#                                   if_else(r1==r2, if_else(r1=="=", r1,"ku"),
-#                                           if_else(r1=="=", if_else(r2=="<",">","<"),
-#                                                   r1 )),
-#                                   if_else(r1==r2,r1,if_else(r1=="=",r2,if_else(r2=="=",r1,"ku"))))))
-#   }
-#   if(is.na(r3)){return()}
-#   if_else(f==g|f==h, paste(e,r3,h, sep = ""), paste(g,r3,f, sep = ""))
-#   }
-# 
-# relationTrain <- function(a){
-#   
-#   b<- as.character(lapply(a,mutualEntail))
-#   trainedRelations <- mutate(data.frame(a), Derivation_Level="Directly Trained",.before = a)
-#   names(trainedRelations)[names(trainedRelations)=="a"]<-"Relation" 
-#   mutuallyDerivedRelations <- mutate(pivot_longer(data.frame(lapply(a,mutualEntail)), cols = starts_with("X"), names_to = "Derivation_Level", values_to = "Relation"), Derivation_Level="Mutually Entailed")
-#   if(length(b)==1){return(rbind(trainedRelations, mutuallyDerivedRelations))}
-#   c<- as.character(if(length(a)<=2){combEntail((combn(a,m=2)))} else(apply(combn(a, m=2),2,combEntail)[!sapply(apply(combn(a, m=2),2,combEntail),is.null)]))
-#   c<- c[!is.na(c)]
-#   q<- unique(apply(combn(c(c,a),m=2),2,combEntail))
-#   q<- as.character(q[2:length(q)])
-#   q<- q[!is.na(q)]
-#   d<- as.character(if(length(unique(c(c,q)))<=1){mutualEntail(c)} else(lapply(c(c,q),mutualEntail)))
-#   
-#   combinatorialDerivedRelations <- data.frame(unique(c(c,q,d)))
-#   names(combinatorialDerivedRelations)[names(combinatorialDerivedRelations)=="unique.c.c..q..d.."]<-"Relation"
-#   combinatorialDerivedRelations<- mutate(combinatorialDerivedRelations,Derivation_Level="Combinatorially Entailed", .before="Relation")
-#   #knownUnknownRelations <-
-#   rbind(trainedRelations,rbind(combinatorialDerivedRelations, mutuallyDerivedRelations))
-# }
-# 
-# 
-# ##Various demonstrations of v0.1 with the test datasets above
-# rel<-relationTrain(a)
-# relb<-relationTrain(b)
-# relc<-relationTrain(c)
-# relt<-relationTrain(t)
-# 
-# #The a,b,c, and t variables below are examples of lists of relational statements.
-#   #You can provide as many statements as you like in the form of Single_letter_Relation(=,<,>, or ku)_Single_letter
-#   #Once your list is saved to a variable name, running relationTrain(variablename) will output a table of relations
-#   # and their general derivation group.
-#   #Currently,longer lists(>3 elements) do not iterate through all possible combinatorially derived relations yet.
-#     #need to write some sort of recursion that adds new relations to q(or c), reruns combEntail,
-#     #and tests the output against the prior state of the list until there are no new statements being generated.
-# 
-# #Long term, it would be nice if this would take statements as an array i.e. [stimuli_name, relation, stimuli_name]
-#   #and thus could handle longer names.
-#   #additionally, adding ability to handle more relational types (i.e. difference, opposition, heirarchy, etc) will add
-#     #functionality to this script.
-# 
-# 
-# #Please leave the below comments.
-#   #this is remnant from testing out the ability to produce a report about the characteristics of the output table
-# 
-# # directTrain<- function(x){
-# #   as.vector(unique(x))
-# # }
-# # 
-# # d<- c("1","2","3","4","2","3")
-# # 
-# # length(directTrain(d))
